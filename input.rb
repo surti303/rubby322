4bf89055ad2fd3f9390340acbf6a08f1cb1aba56
xupycs <kuping.gatel@gmail.com>
2017-05-08 20:48:46 +0800

    kata sandi gan

---
+++ input.rb
@@ -0,0 +1,12 @@
+puts "kon sopo ?"
+nama = gets.chomp
+
+puts "password e opo ?"
+password = gets.chomp
+
+
+if (password == "7737" )
+    puts "kamu sangar" + nama
+else
+    puts "GOBLOK"
+end
---
+++ print.rb
@@ -0,0 +1,8 @@
+nama = "siapa ya?"
+alamat = "dimana ya?"
+
+# print  nama
+# puts  alamat
+
+puts nama
+p alamat
